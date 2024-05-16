8b852db35df5752b8100fee41883c348a0daf492
Ricardokim94 <cmrikim@naver.com>
2024-05-16 10:34:31 +0900

    import / export

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap'
+import memData from './mem.js';
+import { useState } from 'react';
 
 function App() {
+
+  let [member] = useState(memData);
+
   return (
     <div className="App">
 
       {/* NavBar */}
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark" className="navbar-custom">
         <Container>
           <Navbar.Brand href="#home">Oh-Coach Climbing Crew</Navbar.Brand>
           <Nav className="me-auto">
@@ -26,18 +31,18 @@ function App() {
         <div className="row">
           <div className="col-md-4">
             <img src={process.env.PUBLIC_URL + '/창목이.png'} width="90%"/> 
-            <h4>이름</h4>
-            <p>설명</p>
+            <h4>{memData[0].name}</h4>
+            <p>{memData[0].detail}</p>
           </div>
           <div className="col-md-4">
           <img src={process.env.PUBLIC_URL + '/현호.png'} width="90%"/> 
-            <h4>이름</h4>
-            <p>설명</p>
+            <h4>{memData[1].name}</h4>
+            <p>{memData[1].detail}</p>
           </div>
           <div className="col-md-4">
           <img src={process.env.PUBLIC_URL + '/영인이.png'} width="90%"/> 
-            <h4>이름</h4>
-            <p>설명</p>
+            <h4>{memData[2].name}</h4>
+            <p>{memData[2].detail}</p>
           </div>
         </div>
       </div>
---
+++ src/mem.js
@@ -0,0 +1,25 @@
+let memData =
+    [
+        {
+            id: 0,
+            name: "창목",
+            height: 172,
+            detail: "슬랩과 벨런스벽을 좋아함"
+        },
+
+        {
+            id: 1,
+            name: "현호",
+            height: 175,
+            detail: "다이노와 오버행을 좋아함"
+        },
+
+        {
+            id: 2,
+            name: "영인",
+            height: 177,
+            detail: "코디와 그네를 좋아함"
+        }
+    ]
+
+export default memData;
