92def8e3f0df2d72954d79cc5a6682dd166c35e3
Ricardokim94 <cmrikim@naver.com>
2024-05-17 14:42:59 +0900

    react router 사용법 -1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import memData from './mem.js';
 import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+
 
 function App() {
 
@@ -11,6 +13,13 @@ function App() {
 
   return (
     <div className="App">
+      {/* Page 나누기 */}
+      <Routes>
+        <Route path="/" element={<div>Main Page입니다.</div>} />
+        <Route path="/detail" element={<div>상세페이지 입니다.</div>} />
+        <Route />
+      </Routes>
+
 
       {/* NavBar */}
       <Navbar bg="dark" data-bs-theme="dark" className="navbar-custom">
@@ -34,12 +43,12 @@ function App() {
           <Card memData={memData[2]} i={3}></Card>  */}
           {/*반복문 map으로 수정 */}
           {
-            memData.map((a,i)=>{
-              return(
+            memData.map((a, i) => {
+              return (
                 <Card memData={memData[i]} i={i}></Card>
               )
             })
-          }  
+          }
         </div>
       </div>
 
@@ -53,7 +62,7 @@ function Card(props) {
   return (
     <div className="col-md-4">
       {/* 이미지 사진 i번쨰 public 폴더에 있는 것 받아오는 방법 */}
-      <img src={`${process.env.PUBLIC_URL}/mem${props.i+1}.png`} width="90%" />
+      <img src={`${process.env.PUBLIC_URL}/mem${props.i + 1}.png`} width="90%" />
       <h4>{props.memData.name}</h4>
       <p>{props.memData.detail}</p>
     </div>
