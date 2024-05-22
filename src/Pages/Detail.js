fca2ee2f219960a9f903c67904a1700d9d3c8e4d
Ricardokim94 <cmrikim@naver.com>
2024-05-22 15:18:01 +0900

    styled.component

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,17 +1,27 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components"; 
+
+//style
+let Btn = styled.button`
+  background : ${props => props.bg};
+  color : ${props => props.bg == "blue" ? "white" : "black"}; 
+  padding : 10px;
+`
+
+
 
 function Detail(props) {
 
-  let { id } = useParams();
-  console.log('id 값 : ', id);
-  let realId = props.member.find(function(x){ //정렬되었을때 아이디 값 바뀌는것 방지
-    return x.id == id
-  });
+  let { id } = useParams(); 
+  let realId = props.member.find(function(x){ return x.id == id });//정렬되었을때 아이디 값 바뀌는것 방지
   let memberIndex = parseInt(id, 10); // URL의 id를 숫자로 변환 (이미지가 mem1부터 시작한다.)
-
 
   return (
     <div className="container">
+
+        <Btn bg="blue">ddddddd</Btn>
+        <Btn bg="yellow">ddddddd</Btn>
+
       <div className="row">
         <div className="col-md-6">
           <img src={`${process.env.PUBLIC_URL}/mem${memberIndex + 1}.png`} width="100%" alt="상품 이미지" />
