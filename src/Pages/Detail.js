6cbb83a702fda374ea00321348d26331c2f792f2
Ricardokim94 <cmrikim@naver.com>
2024-05-24 17:37:36 +0900

    Tab 버튼구현하기

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,18 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import { Nav } from "react-bootstrap";
 
 function Detail(props) {
+  
+  let { id } = useParams();
+  let memberIndex = parseInt(id, 10); // URL의 id를 숫자로 변환 (이미지가 mem1부터 시작함)
+  let realId = props.member.find(x => x.id === memberIndex); // id를 숫자로 변환하여 비교
+  let [tab, changeTab] = useState(0); // Tab화면 3가지 저장/상태가 3가지니까 숫자로 상태값 설정
 
-  //Lifecycle - Detail 코드가 실행될때 / update 될때 실행 됨.
-  useEffect(()=>{
-    console.log('test')
-  })
 
-
-  let { id } = useParams(); 
-  let realId = props.member.find(function(x){ return x.id === id });//정렬되었을때 아이디 값 바뀌는것 방지
-  let memberIndex = parseInt(id, 10); // URL의 id를 숫자로 변환 (이미지가 mem1부터 시작한다.)
+  // 데이터가 존재하는지 확인
+  if (!realId) { //return 밖에서 if문은 사용 가능하다.
+    return <div>해당 멤버를 찾을 수 없습니다.</div>;
+  }
 
   return (
     <div className="container">
@@ -27,8 +28,42 @@ function Detail(props) {
           <button className="btn btn-danger">같이하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0"> 
+      {/* //defaultActiveKey는 처음 방문했을 때 보여지는 default key */}
+        <Nav.Item>
+          <Nav.Link onClick={()=>{changeTab(0) } }eventKey="link0">버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>{changeTab(1) } } eventKey="link1">버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>{changeTab(2) }} eventKey="link2">버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+       <TabContent tab = {tab}/>
+
+
+
     </div>
   );
+
+  function TabContent({tab}){ //Detail안에 if 문 쓰기 위해서 component 하나 만들어서 안에다가 넣는다.
+    // if(tab == 0){             //props 넣는 것 말고 {이름} 넣으면 이렇게 바로 쓸수 있다. props.tab 안쓰고
+    //   return <div>내용0</div>
+    // }
+    // if(tab == 1){
+    //   return <div>내용1</div>
+    // }
+    // if(tab == 2){
+    //   return <div>내용2</div>
+    // }
+    //if 문 쓰기 싫으면 이렇게도 구현 가능! [ , , , ][0] 은 앞에array에서 0번째 꺼내 달라는 소리임
+    return [<div>내용0</div>,<div>내용1</div>,<div>내용2</div>][tab]
+  }
+
+  
+
 }
 
 export default Detail;
