65d2b8d8f79dec8a196bb0eb22d98bb197bc7f7f
Ricardokim94 <cmrikim@naver.com>
2024-05-24 20:47:57 +0900

    애니메이션 transision 주는방법 (tab기능 효과)

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 
 function Detail(props) {
-  
+
   let { id } = useParams();
   let memberIndex = parseInt(id, 10); // URL의 id를 숫자로 변환 (이미지가 mem1부터 시작함)
   let realId = props.member.find(x => x.id === memberIndex); // id를 숫자로 변환하여 비교
@@ -24,45 +24,52 @@ function Detail(props) {
         <div className="col-md-6">
           <h4 className="pt-5">{realId.name}</h4>
           <p>키 : {realId.height}</p>
-          <p>좋아하는 무브 : {realId.detail}</p>
-          <button className="btn btn-danger">같이하기</button>
+          <p>좋아하는 동작 : {realId.detail}</p>
+          <button className="btn btn-danger">같이할래요 ?</button>
         </div>
       </div>
 
-      <Nav variant="tabs" defaultActiveKey="link0"> 
-      {/* //defaultActiveKey는 처음 방문했을 때 보여지는 default key */}
+      <Nav variant="tabs" defaultActiveKey="link0">
+        {/* //defaultActiveKey는 처음 방문했을 때 보여지는 default key */}
         <Nav.Item>
-          <Nav.Link onClick={()=>{changeTab(0) } }eventKey="link0">버튼0</Nav.Link>
+          <Nav.Link onClick={() => { changeTab(0) }} eventKey="link0">버튼0</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link onClick={()=>{changeTab(1) } } eventKey="link1">버튼1</Nav.Link>
+          <Nav.Link onClick={() => { changeTab(1) }} eventKey="link1">버튼1</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link onClick={()=>{changeTab(2) }} eventKey="link2">버튼2</Nav.Link>
+          <Nav.Link onClick={() => { changeTab(2) }} eventKey="link2">버튼2</Nav.Link>
         </Nav.Item>
       </Nav>
-       <TabContent tab = {tab}/>
+      <TabContent tab={tab} />
 
 
 
     </div>
   );
 
-  function TabContent({tab}){ //Detail안에 if 문 쓰기 위해서 component 하나 만들어서 안에다가 넣는다.
-    // if(tab == 0){             //props 넣는 것 말고 {이름} 넣으면 이렇게 바로 쓸수 있다. props.tab 안쓰고
-    //   return <div>내용0</div>
-    // }
-    // if(tab == 1){
-    //   return <div>내용1</div>
-    // }
-    // if(tab == 2){
-    //   return <div>내용2</div>
-    // }
-    //if 문 쓰기 싫으면 이렇게도 구현 가능! [ , , , ][0] 은 앞에array에서 0번째 꺼내 달라는 소리임
-    return [<div>내용0</div>,<div>내용1</div>,<div>내용2</div>][tab]
+  function TabContent({ tab }) { //Detail안에 if 문 쓰기 위해서 component 하나 만들어서 안에다가 넣는다.
+    //props 넣는 것 말고 {이름} 넣으면 이렇게 바로 쓸수 있다. props.tab 안쓰고
+
+    let [fade, setFade] = useState('')
+
+    useEffect(() => { //state 변할때 end 부착
+      setTimeout(()=> { setFade('end') }, 100) //0.1초 후에 이거 코드 실행
+      
+      return ()=> { //cleanup Function 임. 이게 동작하기전에 동작하는것
+        setFade('') //end 탈부착을 위해 처음에 없앴다가 붙여야 한다.
+      }
+    }, [tab])
+
+    return (
+      //if 문 쓰기 싫으면 이렇게도 구현 가능! [ , , , ][0] 은 앞에array에서 0번째 꺼내 달라는 소리임
+      <div className={'start ' + fade}>  {/* 문자 중간에 변수 넣고 싶으면 */}
+        {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab]}
+      </div>
+    )
   }
 
-  
+
 
 }
 
